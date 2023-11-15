22616f263bc7fa23339673fa1d7f745c82a9005f
Aarom <aarom192@hotmail.com>
2023-11-15 22:16:38 +0800

    2023/11/15(TWN)

---
+++ sendBuf_2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char *sendBuf[20];
+	char msg[] = "1000101CA";
+
+	strcpy(sendBuf, msg);
+	sendBuf += strlen(sendBuf);
+	*sendBuf++=',';
+	*sendBuf++=0;
+    printf("\n");
+    printf("\n");
+  
+
+    printf("src: %s\n", sendBuf);
+    //printf("dest: %s\n", dest);
+	
+	printf("\n");
+    printf("\n");
+    printf("\n");
+
+    return 0;
+}
